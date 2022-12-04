c747f7eb16119c2bb253b38ab654776576728320
Tomara Petty <tomararuth@gmail.com>
2022-12-04 13:35:09 -0800

    adding createBook to add new books into state as an array

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = title => {
-    console.log('need to add book with title of title', title);
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title, },
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
     <div>
+      {books.length}
       <BookCreate onCreate={createBook} />
     </div>
   );
