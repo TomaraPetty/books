db5dfb42667b6c0ae647479bdfd2ef39f5c412ba
Tomara Petty <tomararuth@gmail.com>
2022-12-03 19:20:50 -0800

    adding form to bookcreate with input and button to submit form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
 function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = title => {
+    console.log('need to add book with title of title', title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
