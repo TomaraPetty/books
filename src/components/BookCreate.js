88cf62b8e276c819a8d6da8251bf194d803aaaed
Tomara Petty <tomararuth@gmail.com>
2022-12-04 14:31:25 -0800

    add onDelete to delete books

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -15,7 +15,7 @@ function BookCreate({ onCreate }) {
 
   return (
     <div className='book-create'>
-    <h3>Add a Book</h3>
+      <h3>Add a Book</h3>
       <form onSubmit={handleSubmit}>
         <label>Title</label>
         <input className='input' value={title} onChange={handleChange} />
