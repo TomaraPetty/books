8c23ab055251655b616c07e79782661cf5e98888
Tomara Petty <tomararuth@gmail.com>
2022-12-04 19:51:22 -0800

    add axios and change get books and create books to make api calls instead of hard coding the book object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+
+    setBooks(response.data);
+  };
 
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map(book => {
@@ -24,19 +31,18 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = title => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  const createBook = async title => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
-  const link = "https://picsum.photos"
-
   return (
     <div className='app'>
-    <h1>Reading List</h1>
+      <h1>Reading List</h1>
       <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
