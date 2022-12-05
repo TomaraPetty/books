8e391afd1b9f8e6a11d482fd3d143e52ae370dd0
Tomara Petty <tomararuth@gmail.com>
2022-12-04 20:21:34 -0800

    update edit books by id function to update book info to the entire response.data object instead of just changing the title.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 import axios from 'axios';
@@ -12,10 +12,20 @@ function App() {
     setBooks(response.data);
   };
 
-  const editBookById = (id, newTitle) => {
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    console.log(response);
+
     const updatedBooks = books.map(book => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
